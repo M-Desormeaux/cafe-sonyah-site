d613c0fc17dba9ccb13eaebb1f652599726de8b5
Michael Desormeaux <mdesormeaux96@gmail.com>
2023-02-16 17:58:18 -0600

    baseline website

---
+++ src/content/homepage.js
@@ -0,0 +1,15 @@
+export const aboutMeContent = {
+  heading: "Cafe Sonyah",
+  p1: "Hi there! I'm Sonia, and I'm so excited to welcome you to my website. As a Twitch streamer, I love sharing my gaming experiences with all of you, whether it's exploring the vast world of Skyrim, playing fun party games, or just having a chat with my audience.",
+  p2: "One of the things I love most about streaming is the sense of community it brings. I love getting to know my viewers, hearing their thoughts and opinions, and creating a space where we can all come together and have a great time. Whether you're a longtime fan or a brand new viewer, I'm thrilled to have you here and can't wait to get to know you better.",
+};
+
+export const externalLinks = [
+  { href: "https://www.twitch.tv/cafesonyah", label: "Twitch", newTab: true },
+  { href: "https://discord.gg/z3gZU4XyAH", label: "Discord", newTab: true },
+];
+
+export const footer = {
+  href: "https://mikedez.com",
+  label: "© Meli @ MikeDez.com",
+};
